c32eaa0dc4f57ef729e606256bbfa7080fb5e002
seonyeonghun2 <kjca1117@naver.com>
2024-08-13 14:36:16 +0900

    서버 실행 객체와 포트 분리

---
+++ server/bin/www.js
@@ -0,0 +1,5 @@
+import setting from "../server.js"
+const {app, port} = setting;
+app.listen(port, () => {
+    console.log("Server is running on port 3000");
+  })
--- server/server.js
+++ server/server.js
@@ -49,4 +49,11 @@ app.get('/getAllUsers', function (req, res) {
   //res.send('Hello World')
 })
 
-app.listen(3000)
+// es6 : import(가져오기), export(내보내기)
+// CommonJS : require(가져오기), module.exports 또는 exports (내보내기)
+const port = 3000; // 개발중: 3000, 개발완료: 8080 or 80
+const setting = {
+  app,
+  port
+}
+export default setting;
